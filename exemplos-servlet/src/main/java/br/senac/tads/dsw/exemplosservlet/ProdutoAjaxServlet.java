d77bb279de5677dee682f7654f8e2c24b74f4598
Fernando Tsuda <ftsuda.senac@gmail.com>
2019-03-25 21:57:25 -0300

    Ajustando exemplos

--- exemplos-servlet/src/main/java/br/senac/tads/dsw/exemplosservlet/ProdutoAjaxServlet.java
+++ exemplos-servlet/src/main/java/br/senac/tads/dsw/exemplosservlet/ProdutoAjaxServlet.java
@@ -44,9 +44,6 @@ public class ProdutoAjaxServlet extends HttpServlet {
         try (PrintWriter out = response.getWriter()) {
             out.print(mapper.writeValueAsString(lista));
         }
-        
-        
-        
     }
 
     @Override
