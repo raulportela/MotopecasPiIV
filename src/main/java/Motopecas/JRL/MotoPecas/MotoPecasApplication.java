7fdf10c96852f4ef7c15e79abc292703b4c1bbe7
Raul de Paula <raul.p.p@hotmail.com>
2019-04-06 15:12:50 -0300

    Merge origin/master
    Conflicts:
    	src/main/java/br/senac/tads/pi3a/livrariatades/servico/produto/ProdutosDash.java

---
+++ src/main/java/Motopecas/JRL/MotoPecas/MotoPecasApplication.java
@@ -0,0 +1,13 @@
+package Motopecas.JRL.MotoPecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoPecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoPecasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Motopecas/JRL/MotoPecas/MotoPecasApplicationTests.java
@@ -0,0 +1,16 @@
+package Motopecas.JRL.MotoPecas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MotoPecasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
