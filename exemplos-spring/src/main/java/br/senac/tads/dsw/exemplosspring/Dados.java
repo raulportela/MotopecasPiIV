6b55232e1e1d81782dbf3d9cb616cc5170a28ebc
Fernando Tsuda <ftsuda.senac@gmail.com>
2019-04-08 20:15:50 -0300

    Classe Dados

---
+++ exemplos-spring/src/main/java/br/senac/tads/dsw/exemplosspring/Dados.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.dsw.exemplosspring;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author fernando.tsuda
+ */
+public class Dados implements Serializable {
+
+    private Long id;
+
+    private String nome;
+
+    private String descricao;
+
+    private String email;
+
+    private String senha;
+
+    private int numeroSorte;
+
+    private int sexo;
+
+    private String[] interesses;
+
+    private BigDecimal altura;
+
+    private BigDecimal peso;
+
+    private LocalDate dtNascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getNumeroSorte() {
+        return numeroSorte;
+    }
+
+    public void setNumeroSorte(int numeroSorte) {
+        this.numeroSorte = numeroSorte;
+    }
+
+    public int getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(int sexo) {
+        this.sexo = sexo;
+    }
+
+    public String[] getInteresses() {
+        return interesses;
+    }
+
+    public void setInteresses(String[] interesses) {
+        this.interesses = interesses;
+    }
+
+    public BigDecimal getAltura() {
+        return altura;
+    }
+
+    public void setAltura(BigDecimal altura) {
+        this.altura = altura;
+    }
+
+    public BigDecimal getPeso() {
+        return peso;
+    }
+
+    public void setPeso(BigDecimal peso) {
+        this.peso = peso;
+    }
+
+    public LocalDate getDtNascimento() {
+        return dtNascimento;
+    }
+
+    public void setDtNascimento(LocalDate dtNascimento) {
+        this.dtNascimento = dtNascimento;
+    }
+
+}
