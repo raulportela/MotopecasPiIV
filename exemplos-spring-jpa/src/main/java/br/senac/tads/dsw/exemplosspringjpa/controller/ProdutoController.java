6b27e5ae085769296b62dca7a385cd903520b640
Fernando Tsuda <ftsuda.senac@gmail.com>
2019-04-29 21:21:46 -0300

    Ajustes e remoção

--- exemplos-spring-jpa/src/main/java/br/senac/tads/dsw/exemplosspringjpa/controller/ProdutoController.java
+++ exemplos-spring-jpa/src/main/java/br/senac/tads/dsw/exemplosspringjpa/controller/ProdutoController.java
@@ -8,14 +8,21 @@ package br.senac.tads.dsw.exemplosspringjpa.controller;
 import br.senac.tads.dsw.exemplosspringjpa.entidade.Categoria;
 import br.senac.tads.dsw.exemplosspringjpa.entidade.Produto;
 import br.senac.tads.dsw.exemplosspringjpa.repository.CategoriaRepository;
+import br.senac.tads.dsw.exemplosspringjpa.repository.ProdutoRepository;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -28,11 +35,17 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class ProdutoController {
 
     @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
     private CategoriaRepository categoriaRepository;
 
     @GetMapping
-    public ModelAndView listar() {
-        return new ModelAndView("produto/lista");
+    public ModelAndView listar(
+            @RequestParam(name = "offset", defaultValue = "0") int offset,
+            @RequestParam(name = "qtd", defaultValue = "100") int qtd) {
+        return new ModelAndView("produto/lista").addObject("produtos",
+                produtoRepository.findAll(offset, qtd));
     }
 
     @GetMapping("/novo")
@@ -41,17 +54,46 @@ public class ProdutoController {
     }
 
     @GetMapping("/{id}/editar")
-    public ModelAndView editar(Long id) {
-        return new ModelAndView("produto/formulario").addObject("produto", new Produto());
+    public ModelAndView editar(
+            @PathVariable(name = "id") Long id) {
+        Produto p = produtoRepository.findById(id);
+
+        // Mapeando os IDs das categorias para gerar o formulário com as opções marcadas
+        Set<Integer> idsCategorias = new HashSet<>();
+        for (Categoria cat : p.getCategorias()) {
+            idsCategorias.add(cat.getId());
+        }
+        p.setIdsCategorias(idsCategorias);
+        return new ModelAndView("produto/formulario").addObject("produto", p);
     }
 
     @PostMapping("/salvar")
-    public ModelAndView salvar(Produto produto, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    public ModelAndView salvar(@ModelAttribute("produto") Produto produto,
+            /*BindingResult bindingResult,*/ RedirectAttributes redirectAttributes) {
+        if (produto.getId() == null) {
+            produto.setDtCadastro(LocalDateTime.now());
+        }
+        if (produto.getIdsCategorias() != null && !produto.getIdsCategorias().isEmpty()) {
+            Set<Categoria> categoriasSelecionadas = new HashSet<>();
+            for (Integer idCat : produto.getIdsCategorias()) {
+                Categoria cat  = categoriaRepository.findById(idCat);
+                // Criar a relacao bidirecional entre os objetos produto e categoria.
+                cat.setProdutos(new HashSet<>(Arrays.asList(produto)));
+                categoriasSelecionadas.add(cat);
+            }
+            produto.setCategorias(categoriasSelecionadas);
+        }
+        produtoRepository.save(produto);
+        redirectAttributes.addFlashAttribute("mensagemSucesso",
+                "Produto " + produto.getNome() + " salvo com sucesso");
         return new ModelAndView("redirect:/produto");
     }
 
     @PostMapping("/{id}/remover")
-    public ModelAndView remover(Long id) {
+    public ModelAndView remover(@PathVariable(name="id")Long id, RedirectAttributes redirectAttributes) {
+        produtoRepository.delete(id);
+        redirectAttributes.addFlashAttribute("mensagemSucesso",
+                "Produto ID " + id + " removido com sucesso");
         return new ModelAndView("redirect:/produto");
     }
 
--- exemplos-spring-jpa/src/main/java/br/senac/tads/dsw/exemplosspringjpa/repository/ProdutoRepository.java
+++ exemplos-spring-jpa/src/main/java/br/senac/tads/dsw/exemplosspringjpa/repository/ProdutoRepository.java
@@ -7,15 +7,28 @@ package br.senac.tads.dsw.exemplosspringjpa.repository;
 
 import br.senac.tads.dsw.exemplosspringjpa.entidade.Produto;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author fernando.tsuda
  */
+@Repository
 public class ProdutoRepository {
-    
-    public List<Produto> findAll() {
-        return null;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Produto> findAll(Integer offset, Integer quantidade) {
+        Query jpqlQuery
+                = entityManager.createQuery("SELECT p FROM Produto p")
+                        .setFirstResult(offset)
+                        .setMaxResults(quantidade);
+        return jpqlQuery.getResultList();
     }
 
     public List<Produto> findByCategoria(Integer idCat) {
@@ -23,15 +36,30 @@ public class ProdutoRepository {
     }
 
     public Produto findById(Long id) {
-        return null;
+        Query jpqlQuery
+                = entityManager.createQuery("SELECT p FROM Produto p WHERE p.id = :idProd")
+                        .setParameter("idProd", id);
+        Produto p = (Produto) jpqlQuery.getSingleResult();
+        return p;
     }
 
+    @Transactional
     public void save(Produto p) {
-
+        if (p.getId() == null) {
+            // Salva um novo produto
+            entityManager.persist(p);
+        } else {
+            // Atualiza um produto existente
+            entityManager.merge(p);
+        }
     }
 
+    @Transactional
     public void delete(Long id) {
-
+        // TEM QUE FAZER CONSULTA PARA ESTAR ASSOCIADO AO
+        // ENTITY MANAGER (ATTACHED)
+        Produto p = entityManager.find(Produto.class, id);
+        entityManager.remove(p);
     }
-    
+
 }
